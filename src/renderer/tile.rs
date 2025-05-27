65cdebf1c35e080050f21a9cf08b8a18d3ef794b
darkartx <darkartx@gmail.com>
2025-05-27 20:59:25 +0400

    *

--- src/renderer/tile.rs
+++ src/renderer/tile.rs
@@ -1,16 +1,20 @@
-use bevy_ascii_terminal::{color::{BLACK, GREEN, RED, WHITE}, Glyph, Tile};
+use bevy_ascii_terminal::{
+    color::{BLACK, GREEN, WHITE, GRAY},
+    Glyph,
+    Tile
+};
 
 use crate::{
-    component::{Player, Enemy},
+    component::{Player},
     map::MapTile
 };
 
 pub trait ToTile {
-    fn to_tile(&self) -> Tile;
+    fn to_tile(&self, visible: bool) -> Tile;
 }
 
 impl ToTile for Player {
-    fn to_tile(&self) -> Tile {
+    fn to_tile(&self, _visible: bool) -> Tile {
         Tile {
             glyph: '@',
             fg_color: GREEN,
@@ -19,28 +23,26 @@ impl ToTile for Player {
     }
 }
 
-impl ToTile for Enemy {
-    fn to_tile(&self) -> Tile {
-        Tile {
-            glyph: Glyph::SmilingFace.into(),
-            fg_color: RED,
-            bg_color: BLACK
-        }
-    }
-}
-
 impl ToTile for MapTile {
-    fn to_tile(&self) -> Tile {
+    fn to_tile(&self, visible: bool) -> Tile {
         let glyph = match self {
             MapTile::Void => '.',
             MapTile::Floor => ' ',
             MapTile::Wall => '#',
         };
 
+        let fg_color = if visible {
+            GREEN
+        } else {
+            GRAY
+        };
+
+        let bg_color = BLACK;
+
         Tile {
             glyph,
-            fg_color: WHITE,
-            bg_color: BLACK
+            fg_color,
+            bg_color
         }
     }
 }
